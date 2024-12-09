547e422caa740958a2612a6e72d4b44415ec4a11
Jose-Tejero <j_jtejero@hotmail.com>
2024-12-08 23:50:15 -0600

    Introduciendo movimiento de peón

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import Queen from './icons/queen';
+import QueenMax from './icons/QueenMax';
 import './App.css';
 
 function App() {
@@ -12,18 +12,11 @@ function App() {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0],
   ]);
-
-  console.log(count);
 
   const countUp = (indexRow, indexCol) => {
     const arrayCount = [...count];
-
-    if (arrayCount[indexRow][indexCol] == 1) {
-      arrayCount[indexRow][indexCol]++;
-      setCount(arrayCount);
-    }
 
     if (arrayCount[indexRow][indexCol] == 0) {
       for (let i = 0; i < 8; i++) {
@@ -36,6 +29,12 @@ function App() {
           }
         }
       }
+    } else if (arrayCount[indexRow][indexCol] == 1) {
+      arrayCount[indexRow][indexCol]++;
+      setCount(arrayCount);
+    } else if (arrayCount[indexRow][indexCol] == 2) {
+      arrayCount[indexRow][indexCol]--;
+      setCount(arrayCount);
     }
   };
 
@@ -43,25 +42,37 @@ function App() {
     <>
       <h1>Aprende ajedrez sencillo</h1>
       <div className='card'>
-        {count.map((valueRow, indexRow) =>
-          valueRow.map((valueCol, indexCol) => (
-            <button
-              key={indexRow + indexCol}
-              onClick={() => countUp(indexRow, indexCol)}
-              className={
-                indexRow % 2 != 0
-                  ? indexCol % 2 == 0
-                    ? 'space-dimension'
-                    : 'white-button space-dimension'
-                  : indexCol % 2 == 0
-                  ? 'white-button space-dimension'
-                  : 'space-dimension'
-              }
-            >
-              {valueCol}
-            </button>
-          ))
-        )}
+        {count.map((valueRow, indexRow) => (
+          <div>
+            {valueRow.map((valueCol, indexCol) => (
+              <button
+                key={indexRow + indexCol}
+                onClick={() => countUp(indexRow, indexCol)}
+                className={
+                  indexRow % 2 != 0
+                    ? indexCol % 2 == 0
+                      ? 'space-dimension'
+                      : 'white-button space-dimension'
+                    : indexCol % 2 == 0
+                    ? 'white-button space-dimension'
+                    : 'space-dimension'
+                }
+              >
+                {/* {valueCol == 1 && <Queen />}
+              {valueCol == 2 && <QueenMax style={{fontSize: 20}}/>} */}
+                {/* {valueCol == 1 && 1}
+              {valueCol == 2 && 2} */}
+                {valueCol == 1 ? (
+                  <Queen />
+                ) : valueCol == 2 ? (
+                  <QueenMax style={{ fontSize: 20 }} />
+                ) : (
+                  ''
+                )}
+              </button>
+            ))}
+          </div>
+        ))}
       </div>
     </>
   );
