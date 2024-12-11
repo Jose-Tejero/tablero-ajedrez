55c60c7367447ad5665a406f67f7e8ae89eba205
Jose-Tejero <j_jtejero@hotmail.com>
2024-12-10 20:36:47 -0600

    Fix board order

--- src/icons/Queen.jsx
+++ src/icons/Queen.jsx
@@ -7,6 +7,7 @@ function Queen(props) {
       fill='#FFFFFF'
       height='40px'
       width='40px'
+      display='block'
       version='1.1'
       id='Layer_1'
       xmlns='http://www.w3.org/2000/svg'
--- src/icons/QueenMax.jsx
+++ src/icons/QueenMax.jsx
@@ -7,6 +7,7 @@ function QueenMax(props) {
       fill='#FFFFFF'
       height='60px'
       width='60px'
+      display='block'
       version='1.1'
       id='Layer_1'
       xmlns='http://www.w3.org/2000/svg'
